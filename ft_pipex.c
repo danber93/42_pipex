676545e3e1d59850f60f1f2630ec2e59a61904ad
Luca Iacovelli <liacovel@e4r5p2.42roma.it>
2021-11-06 16:51:17 +0100

    first work

--- ft_pipex.c
+++ ft_pipex.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int openfile(char *filename, int mode)
+int	openfile(char *filename, int mode)
 {
 	if (mode == 0)
 	{
@@ -21,12 +21,71 @@ int openfile(char *filename, int mode)
 				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 }
 
-void redir (char *cmd, char **env, int fdin)
+char	*getPath(char *cmd, char **env)
+{
+	char	*path;
+	char	*dir;
+	char	*bin;
+	int		i;
+	
+	i = 0;
+	while(env[i] && ft_str_ncmp(env[i], "PATH=", 5))
+		i++;
+	if (!env[i])
+		return (cmd);
+	path = env[i] + 5;
+	while (path && ft_pchr(path, ':') > -1);
+	{
+		dir = ft_str_ncmp(path, ft_pchr(path, ':'));
+		bin = path_join(dir, cmd);
+		free(dir);
+		if (access(bin, F_OK) == 0)
+			return (bin);
+		path += ft_pchr(path, ':') + 1;
+	}
+	return (cmd);
+}
+
+void	exec(char *cmd, char **env)
+{
+	char	**args;
+	char	*path;
+
+	// preparazione del args
+	args = ft_str_split(cmd, ' ');
+	// se il primo argomento ha uno '/' vuol dire che é stato specificato e il path, e quindi non serve costruirselo
+	if (ft_pchr(args[0], '/') > -1)
+		path = args[0];
+	else
+		path = getPath(args[0], env);
+	// eseguo il comando
+	execve(path, args, env);
+	write(STDERR, "pipex: ", 7);
+	write(STDERR, cmd, ft_pchr(cmd, 0));
+	write(STDERR, ": command not found\n", 20);
+	exit(127);
+}
+
+void	redir(char *cmd, char **env, int fdin)
 {
 	pid_t	pid;
 	int		pipefd[2];
 
 	pipe(pipefd);
+	pid = fork();
+	if (pid)
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN);
+		waitpid(pid, NULL, 0);
+	}
+	else
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT);
+		// controllare se é necessario if (fdin == STDIN)
+		exec(cmd, env);
+	}
 }
 
 int		main(int ac, char **av, char **env)
@@ -38,12 +97,9 @@ int		main(int ac, char **av, char **env)
 	{
 		fdin = openfile(av[1], INFILE);
 		fdout  = openfile(av[4], OUTFILE);
-		printf("fdin= %i\n", fdin);
-		// replace standard input with input file
   		dup2(fdin, STDIN);
-		// replace standard output with output file
 		dup2(fdout, STDOUT);
 		redir(av[2], env, fdin);
-		printf("fdin= %i\n", fdin);
 	}
+
 }
---
+++ ft_utils.c
@@ -0,0 +1,83 @@
+#include "ft_pipex.h"
+
+int	ft_str_ncmp (char *str1, char *str2, int n)
+{
+	while (--n > 0 && *str1 && *str2 && *str1 == *str2)
+	{
+		str1++;
+		str2++;
+	}
+	return (*str2 - *str1);
+}
+
+int	ft_pchr (char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	if (str[i] == c)
+		return (i);
+	return (-1);
+}
+
+char	*ft_str_ncmp (char *str, unsigned int n)
+{
+	char				*duped;
+	unsigned int		i;
+
+	i = 0;
+	duped = malloc(sizeof(char) * (n + 1));
+	while (i < n)
+		duped[i++] = *str++;
+	duped[n] = 0;
+	return (duped);
+}
+
+char	*path_join (char *path, char *bin)
+{
+	char	*fullPath;
+	int		i;
+	int		j;
+
+	fullPath = malloc(sizeof(char) * (ft_pchr(path, 0)+ ft_pchr(bin, 0) + 2));
+	i = 0;
+	j = 0;
+	while (path[j])
+		fullPath[i++] = path[j++];
+	fullPath[i++] = '/';
+	j = 0;
+	while (bin[j])
+		fullPath[i++] = bin[j++];
+	fullPath[i] = 0;
+	return (fullPath);
+}
+
+char	**ft_str_split (char *str, char div)
+{
+	char	**matrix;
+	int		count;
+	int		i;
+	int		j;
+
+	count = 0;
+	j = 0;
+	while (str[j])
+	{
+		if (str[j++] == div)
+			count++;
+	}
+	matrix = malloc(sizeof(char *) * (count + 2));
+	matrix[count + 1] = NULL;
+	i = 0;
+	while (i < count + 1)
+	{
+		j = 0;
+		while (str[j] && str[j] != div)
+			j++;
+		matrix[i++] = str_ndup(str, j);
+		str = str + j + 1;
+	}
+	return (matrix);
+}
