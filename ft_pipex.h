55a994869f24ec532c6b7b588e319ac442e6e75c
Patrizio Rulli <prulli@e3r9p5.42roma.it>
2021-11-19 15:28:13 +0100

    normed ft_pipex.h

--- ft_pipex.h
+++ ft_pipex.h
@@ -15,12 +15,12 @@
 # define INFILE 0
 # define OUTFILE 1
 
-int	ft_str_ncmp (char *str1, char *str2, int n);
-int	ft_pchr (char *str, char c);
+int		ft_str_ncmp (char *str1, char *str2, int n);
+int		ft_pchr (char *str, char c);
 char	*ft_str_ndup (char *str, unsigned int n);
 char	*path_join (char *path, char *bin);
 char	**ft_str_split (char *str, char div);
-int	ft_str_cmp(char *str1, char *str2);
+int		ft_str_cmp(char *str1, char *str2);
 char*	ft_str_rall(char *buff, char ch);
 int		ft_str_cntn(char *str1, char* str2);
 char	*ft_extract_env_vars(char *buff, char **env);
