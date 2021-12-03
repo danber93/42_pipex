be02ba1bffb162322d4b15966ce910277262b7f2
Patrizio Rulli <prulli@e4r11p4.42roma.it>
2021-12-03 20:46:49 +0100

    second push

--- ft_pipex.h
+++ ft_pipex.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipex.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prulli <prulli@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 20:45:21 by prulli            #+#    #+#             */
+/*   Updated: 2021/12/03 20:46:24 by prulli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PIPEX_H
 # define FT_PIPEX_H
 
@@ -13,14 +25,14 @@
 # define INFILE 0
 # define OUTFILE 1
 
-int		ft_str_ncmp (char *str1, char *str2, int n);
-int		ft_strchr (char *str, char c);
-char	*ft_str_ndup (char *str, unsigned int n);
-char	*ft_join_path (char *path, char *bin);
-char	**ft_str_split (char *str, char div);
+int		ft_str_ncmp(char *str1, char *str2, int n);
+int		ft_strchr(char *str, char c);
+char	*ft_str_ndup(char *str, unsigned int n);
+char	*ft_join_path(char *path, char *bin);
+char	**ft_str_split(char *str, char div);
 int		ft_str_cmp(char *str1, char *str2);
 char	*ft_realloc(char *buff, char ch);
-int		ft_is_substr(char *str1, char* str2);
+int		ft_is_substr(char *str1, char *str2);
 char	*ft_extract_env_vars(char *buff, char **env);
 int		ft_strlen(char *str);
 char	*ft_calloc(size_t size, size_t count);
